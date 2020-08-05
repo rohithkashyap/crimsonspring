a772184044f14afca74bd528c6ff291a6c3c538e
Rohith Kashyap <33201735+rohithkashyap@users.noreply.github.com>
2020-08-05 16:35:14 -0400

    Testing new game

---
+++ Test/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "966827.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 33918348, 33918987 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/33917728/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/33917729/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/33917727/1/ammo.js', 'preload' : true},
+];
